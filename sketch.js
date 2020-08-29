7d46e4b0f9cbb35b540fc0eecf6e11c6443a3a28
vwxwv1 <61165141+vwxwv1@users.noreply.github.com>
2020-08-28 22:55:19 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, box2, box3, box4, box5, pig1, pig2, log1, log2, log3, log4, bird1, backgroundIMG;
+var platform, log6, chain;
+
+function preload(){
+    backgroundIMG = loadImage("sprites/bg.png");
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150,350,300,170);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810,350);
+    log1  = new Log(810,260,300,PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig2 = new Pig(810,220);
+    log2 = new Log(810,180,300,PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log3 = new Log(760,120,150,PI/7);
+    log4 = new Log(870,120,150,-PI/7);
+    
+    bird1 = new Bird(100,100);
+
+    log6 = new Log(200,180,80,PI/2);
+    chain = new Chain(bird1.body,log6.body);
+}
+
+function draw(){
+    background(backgroundIMG);
+    Engine.update(engine);
+
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
+    ground.display();
+    pig1.display();
+    pig2.display();
+    log1.display();
+    log2.display();
+    log3.display();
+    log4.display();
+    bird1.display();
+    platform.display();
+    log6.display();
+    chain.display();
+}
